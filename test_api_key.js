41893baf38717b6e9484c7bcf717a794cc6d192a
avlyalin <lyalin.alexey@gmail.com>
2020-05-29 13:31:34 +0300

    test

--- test_api_key.js
+++ test_api_key.js
@@ -1,4 +1,12 @@
 const fs = require('fs')
 
-const key = process.env.TEST_API_KEY;
-fs.writeFile('test_api_key.txt', key, () => {});
+const vars = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+};
+fs.writeFile('test_api_key.txt', JSON.stringify(vars), () => {});
